5e8dde28d2bad02c2c4525e137f636d015b00f24
jumilvit <jumilvit@yandex.ru>
2020-04-23 17:57:02 +0300

    Машина умеет создавать монеты

--- coffee.js
+++ coffee.js
@@ -3,6 +3,8 @@ let state = "waiting";
 
 let cupImg = document.querySelector(".coffee-cup img");
 let progressBar = document.querySelector(".progress-bar");
+let balanceInput = document.querySelector("input[placeholder='Баланс']");
+
 
 cupImg.onclick = takeCoffee;
 
@@ -11,7 +13,6 @@ function buyCoffee(name, price, element) {
 		return;
 	}
 	//console.log([name, price, element]);
-	let balanceInput = document.querySelector("input[placeholder='Баланс']");
 
 	if (+balanceInput.value < price) {
 		changeDisplayText("Недостаточно средств/Insufficient funds");
@@ -65,4 +66,120 @@ function takeCoffee() {
 function changeDisplayText(text) {
 	let displayText = document.querySelector('.display-text');
 	displayText.innerHTML = text;
+}
+
+//--------------------------Купюры--------------------------
+
+let bills = document.querySelectorAll(".bills img");
+
+for (let i = 0; i < bills.length; i++) {
+	bills[i].onmousedown = takeMoney;
+}
+/* bills[i].onemousedown = function (event) {
+		takeMoney(event);
+}*/
+
+function takeMoney(event) {
+	event.preventDefault(); //alert("Вы нажали на купюру");
+	//console.log(event.target); //console.log(this); равнозначное
+	let bill = event.target;
+
+	bill.style.position = "absolute";
+	bill.style.transform = "rotate(90deg)";
+	bill.style.margin = 0; //сбрасываем отступы
+
+	let billCoords = bill.getBoundingClientRect();
+	let billWidth = billCoords.width;
+	let billHeight = billCoords.height;
+	//console.log(event.clientX, event.clientY); // clientX - захватывает экран без полосы прокрутк, отображает данные по оси Х
+
+	bill.style.top = event.clientY - billWidth/2 + "px";
+	bill.style.left = event.clientX - billHeight/2 + "px";
+
+	window.onmousemove = function(event) {
+		bill.style.top = event.clientY - billWidth/2 + "px";
+		bill.style.left = event.clientX - billHeight/2 + "px";
+		//console.log("Пролетела Мышь!");
+	}
+
+	bill.onmouseup = function() {
+		window.onmousemove = null;
+		if ( inAtm(bill) ){
+			let billCost = +bill.getAttribute("cost");
+			balanceInput.value = +balanceInput.value + billCost;
+			bill.remove();
+		}
+	}
+}
+
+function inAtm(bill) {
+	let atm = document.querySelector(".atm img");
+
+	let atmCoords = atm.getBoundingClientRect();
+	let billCoords = bill.getBoundingClientRect();
+
+	let billLeftTopCorner = {"x" : billCoords.x, "y" : billCoords.y};
+	let billRightTopCorner = {"x" : billCoords.x + billCoords.width, "y" : billCoords.y};
+
+
+	let atmLeftTopCorner = {"x" : atmCoords.x, "y" : atmCoords.y};
+	let atmRightTopCorner = {"x" : atmCoords.x + atmCoords.width, "y" : billCoords.y};
+	let atmLeftBottomCorner = {"x" : atmCoords.x, "y" : billCoords.y + atmCoords.height/3 };
+
+
+	if (billLeftTopCorner.x > atmLeftTopCorner.x
+		&& billRightTopCorner.x < atmRightTopCorner.x
+		&& billLeftTopCorner.y > atmLeftTopCorner.y
+		&& billLeftTopCorner.y < atmLeftBottomCorner.y
+	) {
+	   return true;
+	}  else {
+	   return false;
+		}
+
+}
+
+//СДАЧА
+
+let changeButton = document.querySelector(".change-btn");
+/*changeButton.onclick = function () {
+	takeChange();
+}*/
+changeButton.onclick = takeChange;
+
+function takeChange() {
+	tossCoin("10");
+	/*changeBox.innerHTML += `
+	<img src="img/10rub.png">
+	`;*/
+}
+
+function tossCoin(cost) {
+	let changeBox = document.querySelector(".change-box");
+	changeBox.style.position = "relative";
+
+	let changeBoxCoords = changeBox.getBoundingClientRect();
+	let randomWidth = getRandomInt(0, changeBoxCoords.width - 50);
+	let randomHeight = getRandomInt(0, changeBoxCoords.height - 50);
+
+	let coin = document.createElement("img");
+	coin.setAttribute("src", "img/10rub.png");
+	coin.style.width = "50px"
+	coin.style.height = "50px"
+	changeBox.append(coin);
+	//changeBox.append(coin); // добавляет в конец внутри элемента
+	//changeBox.prepend(coin); // добавляет в начало внутри элемента
+	//changeBox.before(coin); // перед элементом
+	//changeBox.after(coin); // после элемента
+	//changeBox.replaceWith(coin); заменяет элемент
+	coin.style.position = "absolute";
+	coin.style.top = randomHeight + "px";
+	coin.style.left = randomWidth + "px";
+
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
 }
--- index.php
+++ index.php
@@ -8,34 +8,36 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Infant:ital,wght@1,500&family=Exo+2:ital,wght@0,200;0,400;1,300;1,600&family=Lobster&family=Marck+Script&family=Ruslan+Display&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,200;0,400;1,300;1,600&family=Lobster&family=Marck+Script&display=swap" rel="stylesheet">
     <title>Кофе-машина</title>
   </head>
   <body>
     <div class="container">
       <div class="row coffee-machine" id="coffee">
         <div class="col-6 coffee-list d-flex flex-column justify-content-around">
-          <div class="coffee-item" onclick="buyCoffee('Американо/Americano', 50, this)">
+          <div class="coffee-item" onclick="buyCoffee('Американо / Americano', 50, this)">
             <img src="img/americano2.png" alt="">
-            <span>Американо/Americano - 50 &#8381</span>
+            <span>Американо / Americano - 50 &#8381</span>
           </div>
-          <div class="coffee-item" onclick="buyCoffee('Капучино/Cappuccino', 92, this)">
+          <div class="coffee-item" onclick="buyCoffee('Капучино / Cappuccino', 92, this)">
             <img src="img/cappuccino2.png" alt="">
-            <span>Капучино/Cappuccino - 92 &#8381</span>
+            <span>Капучино / Cappuccino - 92 &#8381</span>
           </div>
-          <div class="coffee-item" onclick="buyCoffee('Эспрессо/Espresso', 66, this)">
+          <div class="coffee-item" onclick="buyCoffee('Эспрессо / Espresso', 66, this)">
             <img src="img/espresso2.png" alt="">
-            <span>Эспрессо/Espresso - 66 &#8381</span>
+            <span>Эспрессо / Espresso - 66 &#8381</span>
           </div>
-          <div class="coffee-item" onclick="buyCoffee('Латте/Latte', 128, this)">
+          <div class="coffee-item" onclick="buyCoffee('Латте / Latte', 128, this)">
             <img src="img/latte3.png" alt="">
-            <span>Латте/Latte - 128 &#8381</span>
+            <span>Латте / Latte - 128 &#8381</span>
           </div>
         </div>
         <div class="col-6 coffee-oper">
           <div class="row p-3">
             <div class="col-6">
               <div class="display">
-                <p class="display-text">Выберете Кофе/Choose Coffee</p>
+                <p class="display-text">Выберете Кофе / Choose Coffee</p>
                 <div class="progress">
                   <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width:0%"></div>
                 </div>
@@ -46,7 +48,7 @@
             </div>
             <div class="col-6">
               <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Баланс" aria-label="Имя получателя" aria-describedby="basic-addon2">
+                <input type="text" class="form-control" placeholder="Баланс" aria-label="Имя получателя" aria-describedby="basic-addon2" readonly>
                 <div class="input-group-append">
                   <span class="input-group-text" id="basic-addon2">&#8381</span>
                 </div>
@@ -54,14 +56,18 @@
               <div class="atm">
                 <img src="img/bill_acc.png" alt="">
               </div>
-              <button class="btn btn-primary btn-block mt-3">Сдача/Change</button>
+              <button class="btn btn-primary btn-block mt-3 change-btn">Сдача / Change</button>
               <div class="change-box mt-3"></div>
             </div>
           </div>
         </div>
       </div>
     </div>
-
+    <div class="bills">
+      <img src="img/50rub.jpg" alt="" cost="50">
+      <img src="img/100rub.jpg" alt="" cost="100">
+      <img src="img/500rub.jpg" alt="" cost="500">
+    </div>
     <!-- Optional JavaScript -->
     <script src="script.js"></script>
     <script src="coffee.js"></script>
